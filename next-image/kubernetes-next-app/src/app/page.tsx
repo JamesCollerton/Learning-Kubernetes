a219ad0ccaa02308c7029d5bbc6565b074a49bd6
James Collerton <jamescollerton@live.co.uk>
2025-02-22 22:30:22 +0000

    Push basic Next app

---
+++ next-image/kubernetes-next-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen text-2xl">
+      <div className="m-auto">
+        <h1>
+          Hello, world!
+        </h1>
+      </div>
+    </div>
+  );
+}
